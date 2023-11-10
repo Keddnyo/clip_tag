91551e901880f808477dc68caf4de2a1630da543
Timur Zhdikhanov <65981689+Keddnyo@users.noreply.github.com>
2023-11-11 00:28:24 +0300

    Update show_snackbar.dart

--- lib/utils/show_snackbar.dart
+++ lib/utils/show_snackbar.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void showSnackbar({
   required BuildContext context,
   required String message,
-}) =>
-    ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text(message)));
+}) {
+  ScaffoldMessenger.of(context).clearSnackBars();
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+}
