d0ed7457fcf9fd08998e55bafa001ecf21ca04d5
Timur Zhdikhanov <65981689+Keddnyo@users.noreply.github.com>
2023-11-10 20:09:22 +0300

    New icon

--- lib/core/about_dialog.dart
+++ lib/core/about_dialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart' as material show showAboutDialog;
+import 'package:flutter/material.dart';
 
 import '../shared/constants.dart';
 
@@ -6,5 +7,7 @@ void showAboutDialog(context) => material.showAboutDialog(
       context: context,
       applicationName: Constants.appName,
       applicationVersion: 'Формирование тегов с правилами для 4PDA',
+      applicationIcon: Image.asset('lib/core/assets/app_icon.png',
+          width: 72.0, height: 72.0),
       applicationLegalese: Constants.applicationLegalese,
     );
