40f2207a07481c7ac535cb6f0c415da5a5a59e5f
Timur Zhdikhanov <65981689+Keddnyo@users.noreply.github.com>
2023-11-16 21:05:06 +0300

    Update forum_sections_screen.dart

--- lib/features/forum_sections/ui/forum_sections_screen.dart
+++ lib/features/forum_sections/ui/forum_sections_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../../../shared/bbcode_renderer.dart';
@@ -14,71 +15,111 @@ class ForumSectionsScreen extends StatelessWidget {
     final sectionsController = ForumSectionsController(context: context);
     final scrollController = ScrollController();
 
-    return ListenableBuilder(
-      listenable: sectionsController,
-      builder: (context, child) => Scaffold(
-        appBar: AppBar(
-          title: Text(
-            sectionsController.sections.isNotEmpty
-                ? sectionsController.section!.title
-                : 'ClipTag',
+    return WillPopScope(
+      onWillPop: () async {
+        if (sectionsController.choosenRules.isNotEmpty) {
+          sectionsController.clearChoosenRules();
+          return false;
+        }
+
+        return true;
+      },
+      child: ListenableBuilder(
+        listenable: sectionsController,
+        builder: (context, child) => Scaffold(
+          appBar: AppBar(
+            title: Text(
+              sectionsController.sections.isNotEmpty
+                  ? sectionsController.section!.title
+                  : 'ClipTag',
+            ),
           ),
-        ),
-        body: sectionsController.sections.isNotEmpty
-            ? ListView(
-                controller: scrollController,
-                children: [
-                  for (final category in sectionsController.section!.categories)
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Divider(height: 1.0),
-                        Center(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(category.categoryName),
-                          ),
-                        ),
-                        const Divider(height: 1.0),
-                        for (final rule in category.rules)
-                          ListTile(
-                            title: BBCodeRenderer(content: rule),
-                            onTap: () => sectionsController.navigateToCheckout(
-                              rule: rule,
+          body: sectionsController.sections.isNotEmpty
+              ? ListView(
+                  controller: scrollController,
+                  children: [
+                    for (final category
+                        in sectionsController.section!.categories)
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Divider(height: 1.0),
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(category.categoryName),
                             ),
                           ),
-                      ],
-                    ),
-                ],
-              )
-            : const Center(
-                child: CircularProgressIndicator(),
-              ),
-        endDrawer: sectionsController.sections.isNotEmpty
-            ? NavigationDrawer(
-                onDestinationSelected: (index) {
-                  sectionsController.setSectionIndex(index);
-                  sectionsController.clearChoosenRules();
-                  Navigator.pop(context);
-                  scrollController.jumpTo(0);
-                },
-                selectedIndex: sectionsController.sectionIndex,
-                children: sectionsController.sections
-                    .map(
-                      (section) => NavigationDrawerDestination(
-                        icon: Icon(
-                          section.order == 0
-                              ? Icons.home
-                              : getForumSectionIcon(section.title),
-                        ),
-                        label: Flexible(
-                          child: Text(section.title),
-                        ),
+                          const Divider(height: 1.0),
+                          for (final rule in category.rules)
+                            ListTile(
+                              title: BBCodeRenderer(content: rule),
+                              onTap: () =>
+                                  sectionsController.choosenRules.isNotEmpty
+                                      ? sectionsController.choosenRules
+                                              .contains(rule)
+                                          ? sectionsController.removeRule(rule)
+                                          : sectionsController.addRule(rule)
+                                      : sectionsController.navigateToCheckout(
+                                          rule: rule,
+                                        ),
+                              onLongPress: () =>
+                                  sectionsController.choosenRules.isEmpty
+                                      ? sectionsController.addRule(rule)
+                                      : null,
+                              tileColor:
+                                  sectionsController.choosenRules.contains(rule)
+                                      ? Theme.of(context)
+                                          .colorScheme
+                                          .secondaryContainer
+                                      : null,
+                            ),
+                        ],
                       ),
-                    )
-                    .toList(),
-              )
-            : null,
+                  ],
+                )
+              : const Center(
+                  child: CircularProgressIndicator(),
+                ),
+          endDrawer: sectionsController.sections.isNotEmpty
+              ? NavigationDrawer(
+                  onDestinationSelected: (index) {
+                    sectionsController.setSectionIndex(index);
+                    sectionsController.clearChoosenRules();
+                    Navigator.pop(context);
+                    scrollController.jumpTo(0);
+                  },
+                  selectedIndex: sectionsController.sectionIndex,
+                  children: sectionsController.sections
+                      .map(
+                        (section) => NavigationDrawerDestination(
+                          icon: Icon(
+                            section.order == 0
+                                ? Icons.home
+                                : getForumSectionIcon(section.title),
+                          ),
+                          label: Flexible(
+                            child: Text(section.title),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                )
+              : null,
+          floatingActionButton: FirebaseAuth.instance.currentUser != null
+              ? FloatingActionButton.extended(
+                  onPressed: () {},
+                  icon: Icon(sectionsController.choosenRules.isNotEmpty
+                      ? Icons.bookmark_add_outlined
+                      : Icons.bookmark_outline),
+                  label: Text(
+                    sectionsController.choosenRules.isNotEmpty
+                        ? 'В избранное'
+                        : 'Избранное',
+                  ),
+                )
+              : null,
+        ),
       ),
     );
   }
