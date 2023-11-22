da5f7671d18d82a1144fe9347bca7bb6b0937999
Timur Zhdikhanov <65981689+Keddnyo@users.noreply.github.com>
2023-11-22 18:35:50 +0300

    Update firebase_auth_error_codes.dart

--- lib/shared/firebase/firebase_auth_error_codes.dart
+++ lib/shared/firebase/firebase_auth_error_codes.dart
@@ -1,7 +1,9 @@
 String decodeFirebaseAuthErrorCode(String code) =>
     {
       'invalid-email': 'Неверный логин',
+      'INVALID_LOGIN_CREDENTIALS': 'Неверный логин или пароль',
       'invalid-login-credentials': 'Неверный логин или пароль',
+      'invalid-credential': 'Неверный логин или пароль',
       'email-already-in-use': 'Аккаунт уже зарегистрирован',
       'weak-password': 'Используйте надёжный пароль',
       'too-many-requests': 'Повторите попытку позже',
